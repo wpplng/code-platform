4609639c317d4b96c4c60fc9d5541d24d8a3111e
Lina Väppling <lina.wappling@gmail.com>
2021-01-29 13:58:49 +0100

    Added auth components, and also home and not found page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './assets/scss/app.scss';
+import { Container } from 'react-bootstrap';
+import Login from './components/auth/Login';
+import Logout from './components/auth/Logout';
+import Signup from './components/auth/Signup';
+import ResetPassword from './components/auth/ResetPassword';
+import Home from './components/Home';
+import NotFound from './components/NotFound';
 
-function App() {
+const App = () => {
 	return (
-		<div className='App'>
-			<h1>Code Platform</h1>
-		</div>
+		<Router>
+			<Container className='py-4'>
+				<Routes>
+					<Route path='/'>
+						<Home />
+					</Route>
+
+					<Route path='/signup'>
+						<Signup />
+					</Route>
+
+					<Route path='/login'>
+						<Login />
+					</Route>
+
+					<Route path='/logout'>
+						<Logout />
+					</Route>
+
+					<Route path='/reset-password'>
+						<ResetPassword />
+					</Route>
+
+					<Route path='*' element={<NotFound />} />
+				</Routes>
+			</Container>
+		</Router>
 	);
-}
+};
 
 export default App;
---
+++ src/components/auth/ResetPassword.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ResetPassword = () => {
+	return (
+		<div>
+			<p>ResetPassword component</p>
+		</div>
+	);
+};
+
+export default ResetPassword;
