81d4d4d76762e51176a3de3c7419b3b036c7facc
Lina Väppling <lina.wappling@gmail.com>
2021-03-23 16:12:42 +0100

    Added some temporary content to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Spinner } from 'react-bootstrap';
+import useLanguages from '../hooks/useLanguages';
+import LanguagesGrid from './code/LanguagesGrid';
 
 const Home = () => {
+	const { languages, loading } = useLanguages();
 	return (
 		<div>
-			<p>Home component</p>
+			<h2 className='my-4 text-center'>Code Platform</h2>
+			<p>Description about the page</p>
+
+			{loading ? (
+				<Spinner animation='border' role='status'>
+					<span className='sr-only'>Loading...</span>
+				</Spinner>
+			) : (
+				<LanguagesGrid languages={languages} />
+			)}
 		</div>
 	);
 };
